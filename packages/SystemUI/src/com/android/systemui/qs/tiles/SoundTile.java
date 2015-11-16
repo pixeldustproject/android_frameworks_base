902ed66417b31bbcbe64ceec9c8d2acbf56dcca0
maxwen <max.weninger@gmail.com>
2015-11-17 00:45:25 +0100

    base: silent mode is back Update SoundTile for silent
    ringer mode silent for normal mode and priority interruptions
    
    can be enabled/disabled with
    -volume down/up
    -pressing icon in volume panel
    -global actions menu
    
    Changing ringer mode from silent while in other dnd mode
    will stop dnd except via volume up
    
    PS6:
    -add volume down in silent enters dnd
    
    Change-Id: I368e0dbf6770feb3ad3c39417a0014bf7f4c5cba
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -25,12 +25,10 @@ import android.provider.Settings.Global;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
-import com.android.systemui.statusbar.policy.ZenModeController;
 
 /** Quick settings tile: Sound **/
 public class SoundTile extends QSTile<QSTile.State> {
 
-    private final ZenModeController mZenController;
     private final AudioManager mAudioManager;
 
     private boolean mListening = false;
@@ -40,7 +38,6 @@ public class SoundTile extends QSTile<QSTile.State> {
 
     public SoundTile(Host host) {
         super(host);
-        mZenController = host.getZenModeController();
         mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
         mReceiver = new BroadcastReceiver() {
             @Override
@@ -98,11 +95,10 @@ public class SoundTile extends QSTile<QSTile.State> {
                 break;
             case AudioManager.RINGER_MODE_VIBRATE:
                 newState = AudioManager.RINGER_MODE_SILENT;
-                mZenController.setZen(Global.ZEN_MODE_ALARMS, null, TAG);
+                mAudioManager.setRingerModeInternal(newState);
                 break;
             case AudioManager.RINGER_MODE_SILENT:
                 newState = AudioManager.RINGER_MODE_NORMAL;
-                mZenController.setZen(Global.ZEN_MODE_OFF, null, TAG);
                 mAudioManager.setRingerModeInternal(newState);
                 break;
             default:
@@ -123,7 +119,9 @@ public class SoundTile extends QSTile<QSTile.State> {
                 break;
             case AudioManager.RINGER_MODE_SILENT:
                 state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_silent);
-                state.label = mContext.getString(R.string.quick_settings_sound_dnd);
+                state.label = mContext.getString(R.string.quick_settings_sound_silent);
+                state.contentDescription =  mContext.getString(
+                        R.string.quick_settings_sound_silent);
                 break;
             default:
                 break;
