fbbbf265e8f7a110c441b81c44ed3763dd21e3b9
LorDClockaN <davor@losinj.com>
2016-09-22 12:33:12 +0800

    Powermenu Torch option [1/2] - Ported up by Mazda-- from Lollipop fd77071
    - Use CameraManager instead of TorchManager since we no longer use CM's torch fixes/implementation.
    
    Credit to beanstown106 for his commit here
    49e3cd5
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3891,6 +3891,14 @@ public final class Settings {
         public static final String POWERMENU_POWER = "powermenu_power";
 
         /**
+         * Whether to display the torch option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
+        /**
+
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
