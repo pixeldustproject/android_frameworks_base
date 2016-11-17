d699950982275209ae36e786754c49db5ce338b1
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-11-17 19:40:06 +0800

    [1/2] Lockscreen PIN quick unlock old way does not work anymore exactly like before. So adapt the idea to new LP code
    Thankd DVTonder for the basic idea prior LP
    
    PS:
    - do PIN view correctly
    
    Initial LP implementation by Lars Greiss, ported and adapted to MM by Yank555.lu
    
    Lockscreen PIN quick unlock: Limit quick unlock to a length 4 to avoid lockout
    
    Disabled by default / only for PIN (not password, pointless)
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6443,6 +6443,12 @@ public final class Settings {
         public static final String ASSIST_DISCLOSURE_ENABLED = "assist_disclosure_enabled";
 
         /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
+
+        /**
          * Names of the service components that the current user has explicitly allowed to
          * see all of the user's notifications, separated by ':'.
          *
@@ -6887,6 +6893,7 @@ public final class Settings {
             CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED,
             SYSTEM_NAVIGATION_KEYS_ENABLED,
             QS_TILES,
+            LOCKSCREEN_QUICK_UNLOCK_CONTROL,
             DOZE_ENABLED,
             DOZE_PULSE_ON_PICK_UP,
             DOZE_PULSE_ON_DOUBLE_TAP,
