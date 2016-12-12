29f8b426225e81720cd1e7fcbf48ea22a8c83ce9
Alex Cruz <mazdarider23@gmail.com>
2016-12-12 19:28:51 +0100

    Turn off 'forced battery percent while charging' default [1/2]
    Change-Id: If9a3efe2c2ed129e3bff162a3c9e5b04646899b7
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -347,7 +347,7 @@ public class BatteryMeterDrawable extends Drawable implements
 
     private void updateForceChargeBatteryText() {
         mForceChargeBatteryText = Settings.Secure.getInt(mContext.getContentResolver(),
-                FORCE_CHARGE_BATTERY_TEXT, 1) == 1;
+                FORCE_CHARGE_BATTERY_TEXT, 0) == 1;
     }
 
     private int getColorForLevel(int percent) {
