f998dd879978971a056fa7b4024be64716b5463f
SpiritCroc <spiritcroc@gmail.com>
2017-03-05 17:44:50 +0100

    Slim recents membar: longclick to clear [1/2]
    I know that you can clear all by pinching it...
    but a longclick can be done easier when only using one hand
    
    Change-Id: I61a26882039ff873735a71823a0a4f97cf28f9c7
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4259,11 +4259,18 @@ public final class Settings {
          */
         public static final String SLIM_RECENTS_ICON_PACK = "slim_recents_icon_pack";
 
-	    /**
+        /**
          * show the membar in slim recents
          *  @hide
          */
         public static final String SLIM_RECENTS_MEM_DISPLAY = "slim_recents_mem_display";
+
+        /**
+         * Use the membar in slim recents as clear all button on longclick
+         *  @hide
+         */
+        public static final String SLIM_RECENTS_MEM_DISPLAY_LONG_CLICK_CLEAR =
+                "slim_recents_mem_display_long_click_clear";
 
         /**
          * Whether to use slim recent app sidebar
