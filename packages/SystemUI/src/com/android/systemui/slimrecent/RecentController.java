c99be3b77cceb907fe3f545b65a54f978fb4c90b
SpiritCroc <spiritcroc@gmail.com>
2017-03-31 09:06:57 +0200

    Slim recent membar colors settings: obey themes [1/2]
    Add disabled custom color settings for #00ffffff (fully transparent white).
    http://gerrit.aicp-rom.com/#/c/35222/ says "obey themes when disabled", but
    there was actually no way to disable it. Now theme colors are taken into account
    when user sets color to #00ffffff (just like for the other color settings by slim),
    which is now also the default setting.
    
    Change-Id: Iac9dc6a711cbb58df66cca6de6a0ac1049950fae
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -819,10 +819,9 @@ public class RecentController implements RecentPanelView.OnExitListener,
                     Settings.System.SLIM_RECENTS_MEM_DISPLAY_LONG_CLICK_CLEAR, 0) == 1;
 
             mMembarcolor = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SLIM_MEM_BAR_COLOR, mContext.getResources().getColor(R.color.system_accent_color));
-
+                Settings.System.SLIM_MEM_BAR_COLOR, 0x00ffffff);
             mMemtextcolor = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SLIM_MEM_TEXT_COLOR, mContext.getResources().getColor(R.color.recents_membar_text_color));
+                Settings.System.SLIM_MEM_TEXT_COLOR, 0x00ffffff);
 
         }
     }
@@ -1197,8 +1196,12 @@ public class RecentController implements RecentPanelView.OnExitListener,
             mMemText.setText(String.format(mContext.getResources().getString(R.string.recents_free_ram),available));
             mMemBar.setMax(max);
             mMemBar.setProgress(available);
-            mMemBar.getProgressDrawable().setColorFilter(mMembarcolor, Mode.MULTIPLY); 
-            mMemText.setTextColor(mMemtextcolor);
+            mMemBar.getProgressDrawable().setColorFilter(mMembarcolor == 0x00ffffff
+                    ? mContext.getResources().getColor(R.color.system_accent_color)
+                    : mMembarcolor, Mode.MULTIPLY);
+            mMemText.setTextColor(mMemtextcolor == 0x00ffffff
+                    ? mContext.getResources().getColor(R.color.recents_membar_text_color)
+                    : mMemtextcolor);
     }
 
     public long getTotalMemory() {
