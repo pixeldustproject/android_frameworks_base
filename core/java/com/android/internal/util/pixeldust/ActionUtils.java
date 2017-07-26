9fdc8485a2d285079c3d379a1b963ac2a6a12837
spezi77 <spezi77@gmx.com>
2017-07-26 17:57:19 +0200

    Revert "Hardware key custom rebinding (1/2)"
    This reverts commit 3c9a7904342b9babe7d6f9e9956b1273b746bce8.
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/pixeldust/ActionUtils.java
+++ core/java/com/android/internal/util/pixeldust/ActionUtils.java
@@ -4,13 +4,10 @@ import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
 import android.app.IActivityManager;
-import android.app.usage.UsageStats;
-import android.app.usage.UsageStatsManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.os.Process;
 import android.os.RemoteException;
@@ -21,65 +18,12 @@ import android.view.HapticFeedbackConstants;
 import android.widget.Toast;
 import com.android.internal.R;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class ActionUtils {
     private static final boolean DEBUG = false;
     private static final String TAG = ActionUtils.class.getSimpleName();
     private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
-
-    // Omni Switch Constants
-
-    /**
-     * Package name of the omnniswitch app
-     */
-    public static final String APP_PACKAGE_NAME = "org.omnirom.omniswitch";
-
-    /**
-     * Intent broadcast action for showing the omniswitch overlay
-     */
-    public static final String ACTION_SHOW_OVERLAY = APP_PACKAGE_NAME + ".ACTION_SHOW_OVERLAY";
-
-    /**
-     * Intent broadcast action for hiding the omniswitch overlay
-     */
-    public static final String ACTION_HIDE_OVERLAY = APP_PACKAGE_NAME + ".ACTION_HIDE_OVERLAY";
-
-    /**
-     * Intent broadcast action for toogle the omniswitch overlay
-     */
-    public static final String ACTION_TOGGLE_OVERLAY = APP_PACKAGE_NAME + ".ACTION_TOGGLE_OVERLAY";
-
-    /**
-     * Intent for launching the omniswitch settings actvity
-     */
-    public static Intent INTENT_LAUNCH_APP = new Intent(Intent.ACTION_MAIN)
-            .setClassName(APP_PACKAGE_NAME, APP_PACKAGE_NAME + ".SettingsActivity");
-
-    public static boolean isAppInstalled(Context context, String appUri) {
-        try {
-            PackageManager pm = context.getPackageManager();
-            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public static boolean isAvailableApp(String packageName, Context context) {
-        Context mContext = context;
-        final PackageManager pm = mContext.getPackageManager();
-        try {
-            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
-            int enabled = pm.getApplicationEnabledSetting(packageName);
-            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
-                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
-        } catch (NameNotFoundException e) {
-            return false;
-        }
-    }
 
     /**
      * Kills the top most / most recent user application, but leaves out the launcher.
@@ -89,82 +33,54 @@ public class ActionUtils {
      * @param userId the ID of the currently active user
      * @return {@code true} when a user application was found and closed.
      */
-    public static String killForegroundApp(Context context, int userId) {
+    public static boolean killForegroundApp(Context context, int userId) {
         try {
             return killForegroundAppInternal(context, userId);
         } catch (RemoteException e) {
             Log.e(TAG, "Could not kill foreground app");
         }
-        return null;
+        return false;
     }
 
-    private static String killForegroundAppInternal(Context context, int userId)
+    private static boolean killForegroundAppInternal(Context context, int userId)
             throws RemoteException {
         try {
-            PackageManager packageManager = context.getPackageManager();
             final Intent intent = new Intent(Intent.ACTION_MAIN);
             String defaultHomePackage = "com.android.launcher";
             intent.addCategory(Intent.CATEGORY_HOME);
-            final ResolveInfo res = packageManager.resolveActivity(intent, 0);
+            final ResolveInfo res = context.getPackageManager().resolveActivity(intent, 0);
 
             if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
                 defaultHomePackage = res.activityInfo.packageName;
             }
 
-            // Use UsageStats to determine foreground app
-            UsageStatsManager usageStatsManager = (UsageStatsManager)
-                context.getSystemService(Context.USAGE_STATS_SERVICE);
-            long current = System.currentTimeMillis();
-            long past = current - (1000 * 60 * 60); // uses snapshot of usage over past 60 minutes
-
-            // Get the list, then sort it chronilogically so most recent usage is at start of list
-            List<UsageStats> recentApps = usageStatsManager.queryUsageStats(
-                UsageStatsManager.INTERVAL_DAILY, past, current);
-            Collections.sort(recentApps, new Comparator<UsageStats>() {
-                @Override
-                public int compare(UsageStats lhs, UsageStats rhs) {
-                    long timeLHS = lhs.getLastTimeUsed();
-                    long timeRHS = rhs.getLastTimeUsed();
-                    if (timeLHS > timeRHS) {
-                        return -1;
-                    } else if (timeLHS < timeRHS) {
-                        return 1;
-                     }
-                    return 0;
-                }
-            });
-
             IActivityManager am = ActivityManagerNative.getDefault();
-            if (am.getLockTaskModeState() != ActivityManager.LOCK_TASK_MODE_NONE) return null;
-            // Look for most recent usagestat with lastevent == 1 and grab package name
-            // ...this seems to map to the UsageEvents.Event.MOVE_TO_FOREGROUND
-            String pkg = null;
-            for (int i = 0; i < recentApps.size(); i++) {
-                UsageStats mostRecent = recentApps.get(i);
-                if (mostRecent.mLastEvent == 1) {
-                    pkg = mostRecent.mPackageName;
-                    break;
-                 }
-             }
-
-            if (pkg != null && !pkg.equals("com.android.systemui")
-                    && !pkg.equals(defaultHomePackage)) {
-                am.forceStopPackage(pkg, UserHandle.USER_CURRENT);
-                try {
-                    String appName = (String) packageManager.getApplicationLabel(
-                        packageManager.getApplicationInfo(pkg, PackageManager.GET_META_DATA));
-                    return appName;
-                } catch (PackageManager.NameNotFoundException e) {
-                    // Just use pkg if issues getting appName
-                    return pkg;
+            List<ActivityManager.RunningAppProcessInfo> apps = am.getRunningAppProcesses();
+            for (ActivityManager.RunningAppProcessInfo appInfo : apps) {
+                int uid = appInfo.uid;
+                // Make sure it's a foreground user application (not system,
+                // root, phone, etc.)
+                if (uid >= Process.FIRST_APPLICATION_UID && uid <= Process.LAST_APPLICATION_UID
+                        && appInfo.importance ==
+                        ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                    if (appInfo.pkgList != null && (appInfo.pkgList.length > 0)) {
+                        for (String pkg : appInfo.pkgList) {
+                            if (!pkg.equals("com.android.systemui")
+                                    && !pkg.equals(defaultHomePackage)) {
+                                am.forceStopPackage(pkg, UserHandle.USER_CURRENT);
+                                return true;
+                            }
+                        }
+                    } else {
+                        Process.killProcess(appInfo.pid);
+                        return true;
+                    }
                 }
-            } else {
-                return null;
             }
         } catch (RemoteException remoteException) {
             // Do nothing; just let it go.
         }
-        return null;
+        return false;
     }
 
     /**
